b3a7f6063df2ccd0949e3388b4085e60ae4fa85b
adrian89gh <adrian@dacianempire.com>
2021-07-05 14:43:02 +0300

    add script for console loging if user is on last topic of a module

--- ttw-external-script.js
+++ ttw-external-script.js
@@ -21,13 +21,38 @@ jQuery(function() {
 
 //  <!-- script for changing behavior of Next Lesson button and Mark Complete Button button, when the user is inside a Module -->
 //  <!-- note: inside a Module, and not a sub topic -->
+//  <!-- script for changing behavior of Next Lesson button and Mark Complete Button button, when the user is inside a Module -->
+//  <!-- note: inside a Module, and not a sub topic -->
 jQuery(function() {
     jQuery(".ld-lesson-title").each(function() {
+        //the if clause bellow only executes when the user is viewing viewing a Module page, and also IS on a topic title
+        if ((jQuery(this).css("font-weight") > 550) && (jQuery(".ld-is-current-item").length != 0)) {
+            console.log("inside the last topic")
+
+            // check if there are any topics following the curent one, inside the current module
+            // if there aren't (and the if returns true) than the user is curently on the last topic of a module
+            if (jQuery('.ld-is-current-item').parent().nextAll().length == 0) {
+                jQuery('input[value="Mark Complete"]').on("click", function() {
+                    sessionStorage.setItem("landingFromLastTopic", "True");
+                });
+            }
+        } else {
+            console.log("not inside the last topic")
+        }
+
+
         //the if clause bellow only executes when the user is viewing viewing a Module page, and not on a topic title
         if ((jQuery(this).css("font-weight") > 550) && (jQuery(".ld-is-current-item").length == 0)) {
+            console.log("inside a module, not a topic")
 
             var currentBoldedModule = jQuery(this);
             var firstIncompleteBullet = currentBoldedModule.closest(".ld-lesson-item-preview").next().find(".ld-status-icon.ld-status-incomplete").first();
+
+            if (sessionStorage.getItem("landingFromLastTopic") == "True") {
+                console.log("comming from last topic");
+            } else {
+                console.log("comming from something diferent than last topic");
+            }
 
             if (firstIncompleteBullet.length == 1) {
                 //the line bellow hides the mark complete button for the Module page, if there is even one incomplete subtopic
@@ -36,8 +61,8 @@ jQuery(function() {
                 //the line bellow disables the predefined behavior of the Next lesson button
                 jQuery("span:contains(Next Lesson)").parent().click(false);
 
-                //the code block bellow tells the browser to open a difernt page if the user hits lesson complete
-                jQuery(".ld-button").on("click", function() {
+                //the code block bellow tells the browser to open a difernt page if the user hits Next Lesson
+                jQuery("span:contains(Next Lesson)").parent().on("click", function() {
                     firstIncompleteBullet.click();
                 });
             }
