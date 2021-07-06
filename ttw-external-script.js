a901055d3a0b32e884324545322252ad571da92b
adrian89gh <adrian@dacianempire.com>
2021-07-06 20:23:45 +0300

    fix code for hiding topics when calendar lock icon is present

--- ttw-external-script.js
+++ ttw-external-script.js
@@ -16,7 +16,7 @@ jQuery(function() {
 jQuery(function() {
     jQuery(".ld-lesson-item-preview-heading").each(function() {
         var curentHeading = jQuery(this);
-        if (curentHeading.find(".ld-icon-calendar").length != 0) {
+        if (curentHeading.find(".ld-icon-calendar").length == 0) {
             curentHeading.siblings(".ld-expand-button.ld-button-alternate").css("display", "inline-block");
         }
     });
