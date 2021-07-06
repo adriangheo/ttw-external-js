f3de7a1b14e2574c29b2fb763a81abc8f281f5b5
adrian89gh <adrian@dacianempire.com>
2021-07-06 16:34:05 +0300

    debug script

--- ttw-external-script.js
+++ ttw-external-script.js
@@ -52,12 +52,6 @@ hideCppTopics();
 
 //  <!-- script for changing behavior of Next Lesson button and Mark Complete Button button, when the user is inside a Module -->
 //  <!-- note: inside a Module, and not a sub topic -->
-//  <!-- script for changing behavior of Next Lesson button and Mark Complete Button button, when the user is inside a Module -->
-//  <!-- note: inside a Module, and not a sub topic -->
-jQuery(documet).ready() {
-
-}
-
 jQuery(function() {
     jQuery(".ld-lesson-title").each(function() {
         //the if clause bellow only executes when the user is viewing viewing a Module page, and also IS on a topic title
