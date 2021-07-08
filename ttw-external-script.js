d25e60bc27cda3f654e078df7be477bb8303d033
adrian89gh <adrian@dacianempire.com>
2021-07-08 15:56:34 +0300

    another commit

--- ttw-external-script.js
+++ ttw-external-script.js
@@ -1,6 +1,6 @@
-jQuery(function() {
-    console.log("testing external js");
-});
+// jQuery(function() {
+//     console.log("testing external js");
+// });
 
 
 // <!-- script for hiding checking if each Coaching Certification Program is scheduled (hasn't begun yes)  -->
@@ -17,13 +17,13 @@ jQuery(function() {
 // <!-- and hiding the course expand buttons for the ones that have not yet begun -->
 jQuery(function() {
     jQuery(".ld-lesson-item-preview-heading").each(function() {
-        console.log("test1");
+        // console.log("test1");
         var curentHeading = jQuery(this);
         if (curentHeading.find(".ld-icon-calendar").length == 0) {
-            console.log("test2");
+            // console.log("test2");
             curentHeading.siblings(".ld-expand-button.ld-button-alternate").css("display", "inline-block");
         }
-        console.log("test3");
+        // console.log("test3");
     });
 });
 
@@ -35,12 +35,12 @@ jQuery(function() {
     jQuery(".ld-lesson-title").each(function() {
         //the if clause bellow only executes when the user is viewing viewing a Module page, and also IS on a topic title
         if ((jQuery(this).css("font-weight") > 550) && (jQuery(".ld-is-current-item").length != 0)) {
-            console.log("inside one of the topics")
+            // console.log("inside one of the topics")
 
             // check if there are any topics following the curent one, inside the current module
             // if there aren't (and the if returns true) than the user is curently on the last topic of a module
             if (jQuery('.ld-is-current-item').parent().nextAll().length == 0) {
-                console.log("specificaly, inside the last topic of a module")
+                // console.log("specificaly, inside the last topic of a module")
                 jQuery('input[value="Mark Complete"]').on("click", function() {
                     sessionStorage.setItem("landingFromLastTopic", "True");
                 });
@@ -50,13 +50,13 @@ jQuery(function() {
 
         //the if clause bellow only executes when the user is viewing viewing a Module page, and not on a topic title
         if ((jQuery(this).css("font-weight") > 550) && (jQuery(".ld-is-current-item").length == 0)) {
-            console.log("inside a module, not a topic")
+            // console.log("inside a module, not a topic")
 
             var currentBoldedModule = jQuery(this);
             var firstIncompleteBullet = currentBoldedModule.closest(".ld-lesson-item-preview").next().find(".ld-status-icon.ld-status-incomplete").first();
 
             if (sessionStorage.getItem("landingFromLastTopic") == "True") {
-                console.log("landing from last topic of module, after MarkComplete click");
+                // console.log("landing from last topic of module, after MarkComplete click");
                 sessionStorage.setItem("landingFromLastTopic", "False");
                 //jQuery("span:contains(Next Lesson)").parent().first()[0].click(); //this doesn't work. So I was forced to use to method below
                 if (window.location.href.indexOf("ccp-module-1-introduction") > -1) { //this is actualy the intro
